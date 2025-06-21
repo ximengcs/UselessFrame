2ee31d27c40af9326a9eaaacaf5044867830636c
ximengcs <ximengcs@163.com>
2025-06-21 15:30:14 +0800

    update

--- CSharp/NewRuntime/Net/Utility/NetUtility.cs
+++ CSharp/NewRuntime/Net/Utility/NetUtility.cs
@@ -49,20 +49,19 @@ namespace UselessFrame.Net
 
         internal static MessageTypeInfo GetMessageTypeInfo(string messageTypeFullName)
         {
-            if (_types.TryGetValue(messageTypeFullName, out MessageTypeInfo messageTypeInfo))
-                return messageTypeInfo;
-
             lock (_types)
             {
+                if (_types.TryGetValue(messageTypeFullName, out MessageTypeInfo messageTypeInfo))
+                    return messageTypeInfo;
+
                 ITypeCollection typeMap = X.Type.GetCollection(typeof(IMessage));
                 Type type = typeMap.Get(messageTypeFullName);
                 MessageParser parser = (MessageParser)type.GetProperty("Parser", BindingFlags.Static | BindingFlags.Public).GetValue(null);
                 MessageDescriptor descriptor = (MessageDescriptor)type.GetProperty("Descriptor", BindingFlags.Static | BindingFlags.Public).GetValue(null);
                 messageTypeInfo = new MessageTypeInfo(type, parser, descriptor);
                 _types.TryAdd(messageTypeFullName, messageTypeInfo);
+                return messageTypeInfo;
             }
-
-            return messageTypeInfo;
         }
 
         internal static ServerToken CreateToken(Guid guid)
