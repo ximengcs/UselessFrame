fa1cec81c3ec7c0415bbeb64d6cec5ac6b124275
ximengcs <ximengcs@163.com>
2025-06-25 00:13:22 +0800

    update

--- CSharp/NewRuntime/Fiber/FiberUtility.cs
+++ CSharp/NewRuntime/Fiber/FiberUtility.cs
@@ -1,18 +1,42 @@
 ﻿
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace UselessFrame.NewRuntime.Fiber
 {
     public class FiberUtility
     {
+        public static void RunLoop1(Action<float> handler, CancellationToken token)
+        {
+            RunLoop(0, handler, token);
+        }
+
+        public static void RunLoop0(Action<float> handler, CancellationToken token)
+        {
+            RunLoop(-1, handler, token);
+        }
+
+        public static void RunLoop30(Action<float> handler, CancellationToken token)
+        {
+            RunLoop(30, handler, token);
+        }
+
+        public static void RunLoop60(Action<float> handler, CancellationToken token)
+        {
+            RunLoop(60, handler, token);
+        }
+
         public static void RunLoop(int frameRate, Action<float> handler, CancellationToken token)
         {
             double timestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
-            long ticksPerFrame = TimeSpan.TicksPerSecond / frameRate;
-            long perDeltaTimeStamp = (long)(ticksPerFrame / timestampToTicks);
-
+            long perDeltaTimeStamp = 0;
+            if (frameRate > 0)
+            {
+                long ticksPerFrame = TimeSpan.TicksPerSecond / frameRate;
+                perDeltaTimeStamp = (long)(ticksPerFrame / timestampToTicks);
+            }
             long prevTimestamp = Stopwatch.GetTimestamp();
             while (!token.IsCancellationRequested)
             {
@@ -23,17 +47,24 @@ namespace UselessFrame.NewRuntime.Fiber
 
                 handler(deltaTime);
 
-                SpinWait spin = new SpinWait();
-                long targetTicks = currentTimestamp + perDeltaTimeStamp;
-                while (Stopwatch.GetTimestamp() < targetTicks)
+                if (frameRate > 0)
                 {
-                    long remainingTicks = (long)((targetTicks - Stopwatch.GetTimestamp()) * timestampToTicks);
-                    if (remainingTicks > 4_000_000)
+                    long targetTicks = currentTimestamp + perDeltaTimeStamp;
+                    while (Stopwatch.GetTimestamp() < targetTicks)
+                    {
+                        long remainingTicks = (long)((targetTicks - Stopwatch.GetTimestamp()) * timestampToTicks);
+                        if (remainingTicks > 4_000_000)
+                            Thread.Sleep(1);
+                        else if (remainingTicks > 30_000)
+                            Thread.Sleep(0);
+                    }
+                }
+                else
+                {
+                    if (frameRate == 0)
                         Thread.Sleep(1);
-                    else if (remainingTicks > 50_000)
-                        Thread.Sleep(0);
                     else
-                        spin.SpinOnce();
+                        Thread.Sleep(0);
                 }
             }
         }
