bce9774e94e821fa0db3f798d08843c2d23a9f5b
ximengcs <ximengcs@163.com>
2026-01-14 23:05:23 +0800

    1

--- Unity/Assets/Framework/Scripts/Modules/Resource/Local/ResourcesHelper.AsyncState.cs
+++ Unity/Assets/Framework/Scripts/Modules/Resource/Local/ResourcesHelper.AsyncState.cs
@@ -1,11 +1,29 @@
 ﻿
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
 namespace UselessFrame.ResourceManager
 {
     public partial class ResourcesHelper
     {
         private struct AsyncState
         {
-            
+            private ResourceRequest _request;
+
+            public AsyncState(ResourceRequest request)
+            {
+                _request = request;
+            }
+
+            public async UniTask Load()
+            {
+                _request.completed += CompleteHandler;
+            }
+
+            private void CompleteHandler(AsyncOperation asyncOp)
+            {
+
+            }
         }
     }
 }
